45d84c391880f8148239651924762f8254da669d
Gustavo Alves <gustavowebhostfatec@gmail.com>
2026-01-21 16:41:48 -0300

    feat: exibindo data e hora atuais

--- Program.cs
+++ Program.cs
@@ -1,18 +1,21 @@
 ﻿using ExemploFundamentos.Models;
 
-string apresentacao = "Olá, seja bem vindo!";
-int quantidade = 1;
-quantidade = 10;
-double altura;
-altura = 1.80;
-decimal preco = 1.80M;
-bool condicao = true;
+DateTime dataAtual = DateTime.Now.AddDays(5);
+Console.WriteLine("Data atual: " + dataAtual.ToString("dd/MM/yyyy HH:mm"));
 
-Console.WriteLine(apresentacao);
-Console.WriteLine("Valor da variável quantidade: " + quantidade);
-Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
-Console.WriteLine("Valor da variável preço: "  + preco);
-Console.WriteLine("Valor da variável condicao: " + condicao);
+// string apresentacao = "Olá, seja bem vindo!";
+// int quantidade = 1;
+// quantidade = 10;
+// double altura;
+// altura = 1.80;
+// decimal preco = 1.80M;
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: " + quantidade);
+// Console.WriteLine("Valor da variável altura: " + altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preço: "  + preco);
+// Console.WriteLine("Valor da variável condicao: " + condicao);
 
 // Pessoa p = new Pessoa();
 // p.Nome = "Gustavo";
