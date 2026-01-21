446c314f2b9123afe37748b661e204c5c7dd4ff6
Gustavo Alves <gustavowebhostfatec@gmail.com>
2026-01-21 16:20:00 -0300

    feat: exibição de valores de variáveis no console

--- Program.cs
+++ Program.cs
@@ -1,6 +1,18 @@
 ﻿using ExemploFundamentos.Models;
 
-Pessoa p = new Pessoa();
-p.Nome = "Gustavo";
-p.Idade = 34;
-p.Apresentar();
+string apresentacao = "Olá, seja bem vindo!";
+int quantidade = 1;
+double altura = 1.80;
+decimal preco = 1.80M;
+bool condicao = true;
+
+Console.WriteLine(apresentacao);
+Console.WriteLine("Valor da variável quantidade: " + quantidade);
+Console.WriteLine("Valor da variável altura: " + altura);
+Console.WriteLine("Valor da variável preço: "  + preco);
+Console.WriteLine("Valor da variável condicao: " + condicao);
+
+// Pessoa p = new Pessoa();
+// p.Nome = "Gustavo";
+// p.Idade = 34;
+// p.Apresentar();
